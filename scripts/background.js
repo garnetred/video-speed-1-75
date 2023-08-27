3aa901c8b6bd73e1cd2ffe134cb0481a3fb3d6bd
December G <dgarnetsmith@gmail.com>
2023-08-27 07:10:47 -0500

    Remove logic for iframes

--- scripts/background.js
+++ scripts/background.js
@@ -1,35 +1,18 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   const tabUrl = tab.url ?? tab.pendingUrl;
-  if (changeInfo.status === "complete" && !tabUrl.includes("courses.")) {
-    // const observer = new MutationObserver(mutations => {
-    //   //
-    //   console.log(mutations)
-    // })
-
-    // const video = document.querySelector('video');
-    // console.log(video)
-    // observer.observe(document, {characterData: true})
-    //  try {
-    // document.addEventListener("onload", () => {
-    // console.log("document loaded");
+  if (changeInfo.status === "complete" && tabUrl) {
+    try {
+      chrome.tabs.sendMessage(tabId, {
+        pageLoaded: true,
+        error: null,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  } else {
     chrome.tabs.sendMessage(tabId, {
-      pageLoaded: true,
-      site: undefined,
-      error: null,
-    });
-    console.log("trying to send message");
-    // });
-    // } catch (error) {
-    //   chrome.tabs.sendMessage(tabId, {
-    //     pageLoaded: false,
-    //     error: `The following error has appeared: ${error}`,
-    //   });
-    // }
-  } else if (changeInfo.status === "complete" && tabUrl.includes("courses.")) {
-    chrome.tabs.sendMessage(tabId, {
-      pageLoaded: true,
-      site: "teachable.com",
-      error: null,
+      pageLoaded: false,
+      error: `extension unable to load due to issue with status: ${changeInfo} or tab: ${tabUrl}`,
     });
   }
 });
--- scripts/content.js
+++ scripts/content.js
@@ -1,38 +1,19 @@
 const observer = new MutationObserver((mutations) => {
-  //
   console.log(mutations);
-
-  mutations.forEach((video) => {
-    video.playbackRate = 1.75;
-  });
 });
 
 const videos = document.querySelectorAll("video");
 
 chrome.runtime.onMessage.addListener((obj, sender, response) => {
-  const { pageLoaded, error, site } = obj;
-  if (response && pageLoaded && !site) {
+  const { pageLoaded, error } = obj;
+  if (response && pageLoaded) {
     console.log("response received and page loaded");
     videos.forEach((video) => {
       console.log("video", video);
       observer.observe(video, { attributes: true });
 
       video.playbackRate = 1.75;
-
-      video.addEventListener("paused", (event) => {
-        // video.playbackRate = 1.75;
-        console.log(video.playbackRate, "paused");
-      });
     });
-  } else if (response && pageLoaded && site === "teachable.com") {
-    // window.addEventListener("load", () => {
-    console.log("document loaded");
-    const vjsvideo = document.querySelector("#vjs_video_3_html5_api");
-    console.log(vjsvideo);
-    observer.observe(vjsvideo, { attributes: true, childList: true });
-
-    vjsvideo.playbackRate = 1.75;
-    // });
   } else {
     console.error(error);
   }
